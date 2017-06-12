f54e01f34c307981f85054975ca45f912e359160
peti85g <gerencser.gp.peter@gmail.com>
2017-06-12 05:47:01 +0200

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MostUjraKalkulator
+{
+    public partial class Form1 : Form
+    {
+        double resultValue = 0;
+        string operationPerformed = "";
+        bool isOperationPerformed = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button_clik(object sender, EventArgs e)
+        {
+            if ((textBox1.Text=="0") || (isOperationPerformed))
+            {
+                textBox1.Clear();
+            }
+            Button button=(Button)sender;
+            if (button.Text == ".")
+            {
+                if(textBox1.Text.Contains("."))
+                    textBox1.Text=textBox1.Text;
+            }else
+            textBox1.Text = textBox1.Text + button.Text;
+            isOperationPerformed = false;
+        }
+
+        private void opertaror_click(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+            operationPerformed = button.Text;
+            resultValue = Double.Parse(textBox1.Text);
+            labelCurrentOperation.Text = resultValue + " " + operationPerformed;
+            isOperationPerformed = true;
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "0";
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "0";
+            resultValue = 0;
+        }
+
+        private void button16_Click(object sender, EventArgs e)
+        {
+            switch (operationPerformed)
+            {
+                case "+":
+                    textBox1.Text = (resultValue + Double.Parse(textBox1.Text)).ToString();
+                    break;
+                case "-":
+                     textBox1.Text = (resultValue - Double.Parse(textBox1.Text)).ToString();
+                    break;
+                case "*":
+                      textBox1.Text = (resultValue * Double.Parse(textBox1.Text)).ToString();
+                    break;
+                case "/":
+                       textBox1.Text = (resultValue / Double.Parse(textBox1.Text)).ToString();
+                    break;
+                default:
+                    break;
+
+
+            }
+        }
+    }
+}
